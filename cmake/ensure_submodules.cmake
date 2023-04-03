6fb802b34350a178f5d2496c2e671215392de645
j-hui <j-hui@cs.columbia.edu>
2023-04-03 17:23:33 -0400

    Fix some things

--- cmake/ensure_submodules.cmake
+++ cmake/ensure_submodules.cmake
@@ -1,12 +1,12 @@
 # Update git submodules if user forgot to
 find_package(Git QUIET)
-if(GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
+if(GIT_FOUND AND EXISTS "${CMAKE_SOURCE_DIR}/.git")
     # Update submodules as needed
     option(GIT_SUBMODULE "Check submodules during build" ON)
     if(GIT_SUBMODULE)
         message(STATUS "Submodule update")
         execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init
-                        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
                         RESULT_VARIABLE GIT_SUBMOD_RESULT)
         if(NOT GIT_SUBMOD_RESULT EQUAL "0")
             message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
@@ -15,7 +15,7 @@ if(GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
         # We don't want to use --recursive because pico-sdk contains tinyusb,
         # which contains quite a few submodules that aren't very useful.
         execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init
-                        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/lib/pico-sdk
+                        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/lib/pico-sdk
                         RESULT_VARIABLE GIT_SUBMOD_RESULT)
         if(NOT GIT_SUBMOD_RESULT EQUAL "0")
             message(FATAL_ERROR "git submodule update --init in pico-sdk failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
@@ -23,10 +23,10 @@ if(GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
     endif()
 endif()
 
-if(NOT EXISTS "${PROJECT_SOURCE_DIR}/lib/pico-sdk/CMakeLists.txt")
-    message(FATAL_ERROR "pico-sdk was not downloaded! ${PROJECT_SOURCE_DIR}/lib/pico-sdk/CMakeLists.txt GIT_SUBMODULE was turned off or failed. Please update submodules and try again.")
+if(NOT EXISTS "${CMAKE_SOURCE_DIR}/lib/pico-sdk/CMakeLists.txt")
+    message(FATAL_ERROR "pico-sdk was not downloaded! ${CMAKE_SOURCE_DIR}/lib/pico-sdk/CMakeLists.txt GIT_SUBMODULE was turned off or failed. Please update submodules and try again.")
 endif()
 
-if(NOT EXISTS "${PROJECT_SOURCE_DIR}/lib/ssm-runtime/Makefile")
+if(NOT EXISTS "${CMAKE_SOURCE_DIR}/lib/ssm-runtime/Makefile")
     message(FATAL_ERROR "ssm-runtime was not downloaded! GIT_SUBMODULE was turned off or failed. Please update submodules and try again.")
 endif()
