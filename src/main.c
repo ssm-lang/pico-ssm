c4bb183cb4dc960aefb752218842402949b43379
j-hui <j-hui@cs.columbia.edu>
2023-04-03 16:50:10 -0400

    Add pio to build

--- src/main.c
+++ src/main.c
@@ -1,11 +1,31 @@
-#include <stdio.h>
-#include <pico/stdlib.h>
 #include <ssm-platform.h>
+#include <stdio.h>
+
+#include <hardware/pio.h>
+#include <pico/stdlib.h>
+
+#include "hello.pio.h"
+
+void setup_pio(void) {
+  // Copied from pico-examples
+  PIO pio = pio0;
+  uint offset = pio_add_program(pio, &hello_program);
+  uint sm = pio_claim_unused_sm(pio, true);
+  hello_program_init(pio, sm, offset, PICO_DEFAULT_LED_PIN);
+  while (true) {
+    pio_sm_put_blocking(pio, sm, 1);
+    sleep_ms(500);
+    pio_sm_put_blocking(pio, sm, 0);
+    sleep_ms(500);
+  }
+}
 
 int main(void) {
   stdio_init_all();
 
   printf("Hello world!\n");
+
+  setup_pio();
 
   ssm_platform_entry();
 
