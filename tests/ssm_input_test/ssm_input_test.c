4fca5fcfbbbaa4f5ff0ab5d7ca3163d49630bec3
j-hui <j-hui@cs.columbia.edu>
2023-04-25 16:30:51 -0400

    Change up directory structure to add tests

---
+++ tests/ssm_input_test/ssm_input_test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <pico/stdlib.h>
+#include "ssm.pio.h"
+
+int main(void) {
+    stdio_init_all();
+    printf("Hello world! This is the ssm_input_test\n");
+    return 0;
+}
---
+++ tests/ssm_output_test/ssm_output_test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <pico/stdlib.h>
+
+int main(void) {
+    stdio_init_all();
+    printf("Hello world! This is the ssm_input_test\n");
+    return 0;
+}
