e97b6f022f12b91252702738bb6d52d16bd81d77
j-hui <j-hui@cs.columbia.edu>
2023-04-28 16:02:55 -0400

    Wip

--- tests/ssm_output_test/ssm_output_test.c
+++ tests/ssm_output_test/ssm_output_test.c
@@ -3,6 +3,8 @@
 #include <hardware/clocks.h>
 #include <pico/stdlib.h>
 
+#include "pico/time.h"
+#include "pico/types.h"
 #include "ssm-output.pio.h"
 
 #ifndef PICO_DEFAULT_LED_PIN
@@ -11,11 +13,11 @@
 
 uint32_t clk_sys_hz = 125000000; // Default is (usually) 125MHz (?)
 
-#define ssm_pio_us_to_ticks(us) ((us) * clk_sys_hz / 1000000)
-#define ssm_pio_ticks_to_ctr(ts) (-(ts))
+#define ssm_pio_us_to_ticks(us) ((us) * (clk_sys_hz / 4 / 1000000u))
+#define ssm_pio_ticks_to_ctr(ts) (-((ts)))
 
 #define ssm_us(us) (ssm_pio_ticks_to_ctr(ssm_pio_us_to_ticks(us)))
-#define ssm_ms(ms) ssm_us((ms) * 1000)
+#define ssm_ms(ms) ssm_us((ms) * 1000u)
 
 int main(void) {
   stdio_init_all();
@@ -40,27 +42,37 @@ int main(void) {
   pio_sm_set_enabled(pio0, sm, true);
 
   uint32_t base = 0;
-  const uint32_t epoch = 4000;
+  const uint32_t epoch = 2000;
+
+  absolute_time_t t, t_;
+  uint32_t delta = 0;
 
   // Should loop every 4 seconds, blinking twice with a half-period of 400ms
   while (true) {
 
-    printf("Epoch starting at %u seconds...\n", base);
+    t = get_absolute_time();
+
+    printf("Epoch %d starting at %u microseconds... (delta of last iteration was %u us)\n", base / epoch, base, delta);
 
 #define send(t, v)\
-    printf("    sending: 0x%08x @ %d (%u)\n", v, t, ssm_ms(t)); \
     pio_sm_put(pio0, sm, ssm_ms(t)); \
-    pio_sm_put(pio0, sm, pin_mask); \
+    pio_sm_put(pio0, sm, v); \
 
-    send(base + 400, pin_mask);
-    send(base + 800, 0);
-    send(base + 1200, pin_mask);
-    send(base + 1600, 0);
+    // printf("    sending: 0x%08x @ %d (ticks = %u, ctr = %u)\n", v, t, ssm_pio_us_to_ticks((t) * 1000u), ssm_ms(t)); \
+
+    send(base + 800u, 0);
+    send(base + 1000u, 0xffffffff);
+    send(base + 1200u, 0);
+    send(base + 1600u, 0xffffffff);
 
     printf("Completed epoch...\n");
 
     base += epoch;
-    sleep_ms(epoch);
+
+    t_ = get_absolute_time();
+    delta = (uint32_t) absolute_time_diff_us(t, t_);
+
+    sleep_us(epoch * 1000 - delta);
   }
 
   return 0;
