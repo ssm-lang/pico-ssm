577b5ed0c77334aebd3bb92af643dbcdb8858a4c
Stephen A. Edwards <sedwards@cs.columbia.edu>
2023-05-03 20:46:01 -0400

    Compiling and running extremely limited slang code

--- serial-blink1/pico-tick.c
+++ serial-blink1/pico-tick.c
@@ -20,9 +20,56 @@ static void *alloc_mem(size_t size) { return malloc(size); }
 
 static void free_mem(void *mem, size_t size) { free(mem); }
 
+
+void ssm_throw(ssm_error_t reason, const char *file, int line, const char *func) {
+  printf("Threw error code %d at time: %016llx", reason, ssm_now());
+  switch (reason) {
+  case SSM_INTERNAL_ERROR:
+    printf("Unforeseen internal error\n");
+    break;
+  case SSM_EXHAUSTED_ACT_QUEUE:
+    printf("Tried to insert into full activation record queue\n");
+    break;
+  case SSM_EXHAUSTED_EVENT_QUEUE:
+    printf("Tried to insert into full event queue");
+    break;
+  case SSM_EXHAUSTED_MEMORY:
+    printf("Could not allocate more memory");
+    break;
+  case SSM_EXHAUSTED_PRIORITY:
+    printf("Tried to exceed available recursion depth");
+    break;
+  case SSM_INVALID_TIME:
+    printf(
+        "Invalid time, e.g., scheduled delayed assignment at an earlier time");
+    break;
+  default:
+    printf("Unknown/platform-specific error");
+    break;
+  }
+  exit(reason);
+}
+
+
+
 int ssm_platform_entry(void) {
 
   printf("ssm_platform_entry()\n");
 
+  ssm_mem_init(&alloc_page, &alloc_mem, &free_mem);
+
+  extern ssm_act_t *__enter_main(ssm_act_t *, ssm_priority_t, ssm_depth_t,
+                                 ssm_value_t *, ssm_value_t *);
+  ssm_value_t nothing0 = ssm_new_sv(ssm_marshal(0));
+  ssm_value_t nothing1 = ssm_new_sv(ssm_marshal(0));
+  ssm_value_t main_argv[2] = {nothing0, nothing1};
+
+  /* Start up main routine */
+  ssm_activate(__enter_main(&ssm_top_parent, SSM_ROOT_PRIORITY, SSM_ROOT_DEPTH,
+                            main_argv, NULL));
+
+  ssm_tick();
+
+  
   return 0;
 }
