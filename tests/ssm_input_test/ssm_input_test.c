f02932fa76d8ad3cd2bf8a4bbe5dc24772b8f1bf
KillyBOT <edwards.kyle.j@gmail.com>
2023-05-05 14:58:01 -0400

    Worked more on input

--- tests/ssm_input_test/ssm_input_test.c
+++ tests/ssm_input_test/ssm_input_test.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "hardware/gpio.h"
 #include "hardware/pio.h"
-#include "ssm_input.pio.h"
 #include <hardware/clocks.h>
 #include <hardware/gpio.h>
 #include <pico/stdlib.h>
+#include "ssm-input.pio.h"
+#include "ssm-timer.pio.h"
 
 #define PIN_IN 22
 #define PIN_OUT PICO_DEFAULT_LED_PIN
@@ -16,14 +17,12 @@ uint32_t read_gpio(void) {
   return pin_mask & gpio_get_all();
 }
 
-
 int main(void) {
 
-    uint32_t rx_fifo_out;
+    uint32_t input_rx_fifo_out, timer_rx_fifo_out;
+    int input_sm, timer_sm;
 
     stdio_init_all();
-
-    sleep_ms(10);
 
     printf("\n\n=== ssm_input_test ===\n");
 
@@ -34,15 +33,22 @@ int main(void) {
 
     printf("Current GPIO situation: %08x\n", read_gpio());
 
-    gpio_set_pulls(PIN_IN, true, false);
-    ssm_input_init(pio0, PIN_IN, PIN_OUT);
+    gpio_set_pulls(PIN_IN, true, false); //Set to pull-up
+    input_sm = ssm_input_program_start(pio0, PIN_IN, 32);
+    timer_sm = ssm_pio_timer_program_start(pio0);
+
+    pio_sm_set_enabled(pio0, timer_sm, true); //Enable timer
 
     printf("Enabled state machines\n");
     printf("Current GPIO situation: %08x\n", read_gpio());
 
     while (1) {
-        rx_fifo_out = pio_sm_get_blocking(pio0, 0);
-        printf("Input switched to %u at time %llu\n", rx_fifo_out, time_us_64());
+        if (pio_interrupt_get(pio0, SSM_INPUT_IRQ_NUM)) {
+            pio_interrupt_clear(pio0, SSM_INPUT_IRQ_NUM);
+            timer_rx_fifo_out = ssm_pio_timer_read(pio0, timer_sm);
+            input_rx_fifo_out = pio_sm_get(pio0, input_sm);
+            printf("New input %lu at time %lu\n", input_rx_fifo_out, timer_rx_fifo_out);
+        }
     }
 
     return 0;
