5ddbc8352534cac6eb85cf1754e6de7a97afc7ab
j-hui <j-hui@cs.columbia.edu>
2023-05-20 23:32:00 -0400

    Adjust C implementation of pulse100us to match sslang impl

--- tests/pulse100us-c_test/main.c
+++ tests/pulse100us-c_test/main.c
@@ -3,8 +3,8 @@
 #include <hardware/clocks.h>
 #include "pico/stdlib.h"
 
-#define INPUT_PIN 22
-#define OUTPUT_PIN 0
+#define INPUT_PIN 14
+#define OUTPUT_PIN 15
 #define SLEEP_TIME_US 100
 
 void gpio_callback(uint gpio, uint32_t events) {
@@ -22,12 +22,11 @@ int main() {
 
     gpio_init(INPUT_PIN);
     gpio_set_dir(INPUT_PIN, GPIO_IN);
-    gpio_pull_up(INPUT_PIN);
+    // gpio_pull_up(INPUT_PIN); // Only for physical buttons
     gpio_set_input_enabled(INPUT_PIN, true);
 
-    gpio_set_irq_enabled_with_callback(INPUT_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(INPUT_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
 
     for (;;)
         tight_loop_contents();
-
 }
