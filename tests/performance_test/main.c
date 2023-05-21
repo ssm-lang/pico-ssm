64232f644520b0939edd24640b1a6e85b561f072
KillyBOT <edwards.kyle.j@gmail.com>
2023-05-20 22:07:48 -0400

    Wrote perf test in C with GPIO library

--- tests/performance_test/main.c
+++ tests/performance_test/main.c
@@ -1,52 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "hardware/gpio.h"
 #include <hardware/clocks.h>
-#include <pico/stdlib.h>
+#include "pico/stdlib.h"
 
 #define INPUT_PIN 22
 #define OUTPUT_PIN 0
 #define SLEEP_TIME_US 100
 
-int main() {
+void gpio_callback(uint gpio, uint32_t events) {
+    gpio_put(OUTPUT_PIN, 1);
+    busy_wait_us(SLEEP_TIME_US);
+    gpio_put(OUTPUT_PIN, 0);
+}
 
-    uint32_t start_time, delta_time, total_time, button_presses;
+int main() {
 
     stdio_init_all();
 
     gpio_init(OUTPUT_PIN);
     gpio_set_dir(OUTPUT_PIN, GPIO_OUT);
 
-    gpio_init (INPUT_PIN);
+    gpio_init(INPUT_PIN);
     gpio_set_dir(INPUT_PIN, GPIO_IN);
     gpio_pull_up(INPUT_PIN);
     gpio_set_input_enabled(INPUT_PIN, true);
 
-    total_time = 0;
-    button_presses = 0;
+    gpio_set_irq_enabled_with_callback(INPUT_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
-    for (;;) {
-
-        //Spin and wait for the input to be enabled
-        while (gpio_get(INPUT_PIN))
-            ;
-
-        // Send a 100us pulse
-        start_time = time_us_32();
-        gpio_put(OUTPUT_PIN, 1);
-        sleep_us(SLEEP_TIME_US);
-        gpio_put(OUTPUT_PIN, 0);
-        delta_time = time_us_32() - start_time;
-        button_presses++;
-        total_time += delta_time;
-
-        //Print the results
-        printf("Pulse len: %lu Avg. pulse len: %lu\n", delta_time, total_time / button_presses);
-
-        //Wait for the button to reset
-        while (!gpio_get(INPUT_PIN))
-            ;
-
-    }
+    for (;;)
+        tight_loop_contents();
 
 }
